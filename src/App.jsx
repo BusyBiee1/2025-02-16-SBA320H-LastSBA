c8babdf84504e55020286a043d8527fedee10458
busybiee1 <busybiee1@gmail.com>
2025-02-16 05:20:36 -0600

    get movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieDetails';
 import './App.css';
-
 
 
 export default app;
---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function MovieDetails() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+    //const API_KEY = "cd171968";
