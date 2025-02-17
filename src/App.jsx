05eb245894f615c7bf523c6db58de62906a1cb0c
busybiee1 <busybiee1@gmail.com>
2025-02-16 18:35:25 -0600

    debugging stuck

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieList';
 import MovieDetails from './components/MovieDetails';
 import './App.css';
 
